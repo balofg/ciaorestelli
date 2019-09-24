c4eaab55f26b13d0b59238d66a651633e9212cad
Barnaba Longo <blongo@cuebiq.com>
2019-09-24 18:57:46 +0200

    mother forgive me for the nested try/catches

--- src/lambdas/post/index.js
+++ src/lambdas/post/index.js
@@ -6,69 +6,46 @@ module.exports = {
   handler: async () => {
     const userToken = await dynamo.getSetting(dynamo.settings.USER_TOKEN);
 
-    try {
-      const pagesListResponse = await fb.listPages(userToken);
+    const pagesListResponse = await fb.listPages(userToken);
 
-      if (!pagesListResponse.data) {
-        console.error("page list error", pageListResponse);
-        throw new Error("page list error");
-      }
-
-      const page = pagesListResponse.data.find(
-        page => page.id === process.env.page_id
-      );
-
-      if (!page) {
-        console.error(
-          "page error",
-          `cannot find page with id ${process.env.page_id}`
-        );
-        throw new Error("page error");
-      }
-
-      try {
-        const pageTokenResponse = await fb.getPageAccessToken(
-          page.id,
-          userToken
-        );
-
-        if (!pageTokenResponse.data) {
-          console.error("page token error", pageTokenResponse);
-          throw new Error("page token error");
-        }
-
-        const pageToken = pageTokenResponse.data.access_token;
-
-        try {
-          const randomPersonResponse = await people.getPerson();
-
-          if (!randomPersonResponse.data) {
-            console.error("random person error", randomPersonResponse);
-            throw new Error("random person error");
-          }
-
-          const person = randomPersonResponse.data;
-          const message = `${person.name} ${person.surname} non saluta Restelli.`;
-
-          try {
-            await fb.postToPage(page.id, message, pageToken);
-            // success
-            console.info(message);
-          } catch (postError) {
-            console.error("post error", postError);
-            throw new Error("post error");
-          }
-        } catch (randomPersonError) {
-          console.error("random person error", randomPersonError);
-          throw new Error("random person error");
-        }
-      } catch (pageTokenError) {
-        console.error("page token error", pageTokenError);
-        throw new Error("page token error");
-      }
-    } catch (pageListError) {
-      console.error("page list error", pageListError);
+    if (!pagesListResponse.data) {
+      console.error("page list error", pageListResponse);
       throw new Error("page list error");
     }
+
+    const page = pagesListResponse.data.data.find(
+      page => page.id === process.env.page_id
+    );
+
+    if (!page) {
+      console.error(
+        "page error",
+        `cannot find page with id ${process.env.page_id}`
+      );
+      throw new Error("page error");
+    }
+
+    const pageTokenResponse = await fb.getPageAccessToken(page.id, userToken);
+
+    if (!pageTokenResponse.data) {
+      console.error("page token error", pageTokenResponse);
+      throw new Error("page token error");
+    }
+
+    const pageToken = pageTokenResponse.data.access_token;
+
+    const randomPersonResponse = await people.getPerson();
+
+    if (!randomPersonResponse.data) {
+      console.error("random person error", randomPersonResponse);
+      throw new Error("random person error");
+    }
+
+    const person = randomPersonResponse.data;
+    const message = `${person.name} ${person.surname} non saluta Restelli.`;
+
+    await fb.postToPage(page.id, message, pageToken);
+    // success
+    console.info(message);
   }
 };
