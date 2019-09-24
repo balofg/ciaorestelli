c284e348bd215e2d85d3e8eb9b374d6ff7775a9b
Barnaba Longo <blongo@cuebiq.com>
2019-09-24 15:38:53 +0200

    hide api-gateway url, you fucking idiot

--- src/lambdas/auth/index.js
+++ src/lambdas/auth/index.js
@@ -4,7 +4,7 @@ exports.handler = async event => {
       return {
         statusCode: 301,
         headers: {
-          Location: `https://www.facebook.com/v4.0/dialog/oauth?client_id=${process.env.client_id}&state=appstate123&scope=manage_pages&redirect_uri=https://ur7a2a7vgj.execute-api.us-east-1.amazonaws.com/prod/auth/callback`
+          Location: `https://www.facebook.com/v4.0/dialog/oauth?client_id=${process.env.client_id}&state=appstate123&scope=manage_pages&redirect_uri=${process.env.redirect_uri}`
         }
       };
     }
